a8ba4560f13a22ded3f7fe99d35aaa68c50b4301
Shlok Ramteke <shlok.ramteke24@gmail.com>
2024-01-16 17:03:47 +0530

    Add enemy and start screen

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Sky from './Sky';
-import Ground from './Ground';
-import CannonBase from './CannonBase';
-import CannonPipe from './CannonPipe';
+import React from "react";
+import PropTypes from "prop-types";
+import Sky from "./Sky";
+import Ground from "./Ground";
+import CannonBase from "./CannonBase";
+import CannonPipe from "./CannonPipe";
 import CannonBall from "./CannonBall";
+import CurrentScore from "./CurrentScore";
+import FlyingObject from "./FlyingObject";
+import Heart from "./Heart";
+import StartGame from "./StartGame";
 
 const Canvas = (props) => {
-  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+  const viewBox = [
+    window.innerWidth / -2,
+    100 - window.innerHeight,
+    window.innerWidth,
+    window.innerHeight,
+  ];
   return (
     <svg
       id="aliens-go-home-canvas"
@@ -15,11 +24,21 @@ const Canvas = (props) => {
       onMouseMove={props.trackMouse}
       viewBox={viewBox}
     >
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
       <Sky />
       <Ground />
       <CannonPipe rotation={props.angle} />
       <CannonBase />
-      <CannonBall position={{x: 0, y: -100}}/>
+      <CannonBall position={{ x: 0, y: -100 }} />
+      <CurrentScore score={15} />
+      <FlyingObject position={{x: -150, y: -300}}/>
+      <FlyingObject position={{x: 150, y: -300}}/>
+      <Heart position={{x: -300, y: 35}} />
+      <StartGame onClick={() => console.log('Aliens, Go Home!')} />
     </svg>
   );
 };
